f1ea8dade3e99828abb436eff60c499979ed1f2a
Alyona <alyona-gra98@yandex.ru>
2017-12-19 01:17:56 +0300

    tests with member of class pointer

--- test/test_fourier_transform.cpp
+++ test/test_fourier_transform.cpp
@@ -3,6 +3,9 @@
 #include "fourier_transformation.h"
 #include "constants.h"
 
+typedef double (vec3<double>::*MethodCoord)() const;
+typedef vec3<double> node::* MemberOfNode;
+
 class TestFourierTransform : public testing::Test {
 public:
 	int nx = 4, ny = 5, nz = 6;
@@ -14,14 +17,50 @@ public:
 		for (int i = 0; i <= nx; i++)
 			for (int j = 0; j <= ny; j++)
 				for (int k = 0; k <= nz; k++) {
-					double x = A * sin(2 * constants::pi*i / nx);
-					double y = A * sin(2 * constants::pi*i / nx);
-					double z = A * sin(2 * constants::pi*i / nx);
+					double x = A * sin(2 * constants::pi*k / nx);
+					double y = A * sin(2 * constants::pi*i / nz);
+					double z = A * sin(2 * constants::pi*j / ny);
 
 					grid(i, j, k).E[0] = grid(i, j, k).B[0] = x;
 					grid(i, j, k).E[1] = grid(i, j, k).B[1] = y;
 					grid(i, j, k).E[2] = grid(i, j, k).B[2] = z;
 				}
+	}
+
+	MemberOfNode GetField(int f) {
+		switch (f) {
+		case 0:
+			return &node::E;
+		case 1:
+			return &node::B;
+		}
+	}
+
+	MethodCoord GetMethod(int coord) {
+		switch (coord) {
+		case 0:
+			return &vec3<double>::x;
+		case 1:
+			return &vec3<double>::y;
+		case 2:
+			return &vec3<double>::z;
+		}
+	}
+ 
+	void MyTestBody(Field field) {
+
+		MemberOfNode p = GetField(field/3);
+		MethodCoord m= GetMethod(field % 3);
+
+		Grid3d grid2 = grid;
+
+		FourierTransformation(grid, field, RtoC);
+		FourierTransformation(grid, field, CtoR);
+
+		for (int i = 0; i <= grid.gnxCells(); i++)
+			for (int j = 0; j <= grid.gnyCells(); j++)
+				for (int k = 0; k <= grid.gnzCells(); k++)
+					ASSERT_NEAR((grid(i, j, k).*p.*m)(), (grid2(i, j, k).*p.*m)(), 0.1);
 	}
 };
 
@@ -30,68 +69,25 @@ TEST_F(TestFourierTransform, no_throws) {
 }
 
 TEST_F(TestFourierTransform, transform_correctly_Ex) {
-	Grid3d grid2 = grid;
-
-	FourierTransformation(grid, Ex, RtoC);
-	FourierTransformation(grid, Ex, CtoR);
-
-	for (int i = 0; i <= grid.gnxCells(); i++)
-		for (int j = 0; j <= grid.gnyCells(); j++)
-			for (int k = 0; k <= grid.gnzCells(); k++)
-				ASSERT_NEAR(grid(i, j, k).E.x(), grid2(i, j, k).E.x(), 0.1);
+	MyTestBody(Ex);
 }
 
 TEST_F(TestFourierTransform, transform_correctly_Ey) {
-	Grid3d grid2 = grid;
-	FourierTransformation(grid, Ey, RtoC);
-	FourierTransformation(grid, Ey, CtoR);
-
-	for (int i = 0; i <= grid.gnxCells(); i++)
-		for (int j = 0; j <= grid.gnyCells(); j++)
-			for (int k = 0; k <= grid.gnzCells(); k++)
-				ASSERT_NEAR(grid(i, j, k).E.y(), grid2(i, j, k).E.y(), 0.1);
+	MyTestBody(Ey);
 }
 
 TEST_F(TestFourierTransform, transform_correctly_Ez) {
-	Grid3d grid2 = grid;
-	FourierTransformation(grid, Ez, RtoC);
-	FourierTransformation(grid, Ez, CtoR);
-
-	for (int i = 0; i <= grid.gnxCells(); i++)
-		for (int j = 0; j <= grid.gnyCells(); j++)
-			for (int k = 0; k <= grid.gnzCells(); k++)
-				ASSERT_NEAR(grid(i, j, k).E.z(), grid2(i, j, k).E.z(), 0.1);
+	MyTestBody(Ez);
 }
 
 TEST_F(TestFourierTransform, transform_correctly_Bx) {
-	Grid3d grid2 = grid;
-	FourierTransformation(grid, Bx, RtoC);
-	FourierTransformation(grid, Bx, CtoR);
-
-	for (int i = 0; i <= grid.gnxCells(); i++)
-		for (int j = 0; j <= grid.gnyCells(); j++)
-			for (int k = 0; k <= grid.gnzCells(); k++)
-				ASSERT_NEAR(grid(i, j, k).B.x(), grid2(i, j, k).B.x(), 0.1);
+	MyTestBody(Bx);
 }
 
 TEST_F(TestFourierTransform, transform_correctly_By) {
-	Grid3d grid2 = grid;
-	FourierTransformation(grid, By, RtoC);
-	FourierTransformation(grid, By, CtoR);
-
-	for (int i = 0; i <= grid.gnxCells(); i++)
-		for (int j = 0; j <= grid.gnyCells(); j++)
-			for (int k = 0; k <= grid.gnzCells(); k++)
-				ASSERT_NEAR(grid(i, j, k).B.y(), grid2(i, j, k).B.y(), 0.1);
+	MyTestBody(By);
 }
 
 TEST_F(TestFourierTransform, transform_correctly_Bz) {
-	Grid3d grid2 = grid;
-	FourierTransformation(grid, Bz, RtoC);
-	FourierTransformation(grid, Bz, CtoR);
-
-	for (int i = 0; i <= grid.gnxCells(); i++)
-		for (int j = 0; j <= grid.gnyCells(); j++)
-			for (int k = 0; k <= grid.gnzCells(); k++)
-				ASSERT_NEAR(grid(i, j, k).B.z(), grid2(i, j, k).B.z(), 0.1);
+	MyTestBody(Bz);
 }
